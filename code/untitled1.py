a50a7f3d697a9613cc981a3491ec5b2c6d78757d
Emiliyan Pavlov <pavlov.e1998@gmail.com>
2019-09-13 19:16:29 +0200

    Linkage of rain to station areas (There are also small data problems, read on it further and play with code)

--- code/untitled1.py
+++ code/untitled1.py
@@ -6,4 +6,4 @@ Created on Thu Sep 12 20:51:39 2019
 """
 import pandas as pd
 path4_linkinfo = "C:/Users/Hermii/Desktop/Data Challenge 3/repo/waterschap-aa-en-maas_sewage_2019/sewer_model"
-print(pd.read_csv(path4_linkinfo+"/20180717_dump riodat rioleringsdeelgebieden_matched_to_rainfall_locations.csv").head(100))
+pd.read_csv(path4_linkinfo+"/20180717_dump riodat rioleringsdeelgebieden_matched_to_rainfall_locations.csv")
